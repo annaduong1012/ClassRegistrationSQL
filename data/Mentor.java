973eb55a24b6405b6f890ce21797f275c79599b8
Anna Duong <111554660+annaduong1012@users.noreply.github.com>
2023-12-20 23:43:27 +1100

    Update Mentor.java

--- data/Mentor.java
+++ data/Mentor.java
@@ -4,13 +4,11 @@ public class Mentor {
 	String name;
 	String expertise;
 	String workPlace;
-	
+
 	public Mentor() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	
+
 	public Mentor(String name, String expertise, String workPlace) {
 		super();
 		this.name = name;
@@ -18,26 +16,28 @@ public class Mentor {
 		this.workPlace = workPlace;
 	}
 
-
-
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getExpertise() {
 		return expertise;
 	}
+
 	public void setExpertise(String expertise) {
 		this.expertise = expertise;
 	}
+
 	public String getWorkPlace() {
 		return workPlace;
 	}
+
 	public void setWorkPlace(String workPlace) {
 		this.workPlace = workPlace;
 	}
-	
-	
+
 }
